4d87cb735c35e9f36d99a87c7a4b027d7f0e2072
germa <germa@DESKTOP-JO4JQIV>
2023-05-10 20:13:46 -0300

    ma

--- src/main/java/com/portfolio/tk/controller/NavbarController.java
+++ src/main/java/com/portfolio/tk/controller/NavbarController.java
@@ -2,17 +2,17 @@ package com.portfolio.tk.controller;
 
 import com.portfolio.tk.entity.Navbar;
 import com.portfolio.tk.service.NavbarService;
+import java.sql.SQLException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import java.sql.SQLException;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 @RestController
 @RequestMapping("/navbar")
